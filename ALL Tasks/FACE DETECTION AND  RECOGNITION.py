c2c52dce04e347a0d4f52ecfd03c8d9dce2b68d2
shrutijaiswal2905 <157979575+shrutijaiswal2905@users.noreply.github.com>
2024-03-16 18:29:13 +0530

    Update FACE DETECTION AND  RECOGNITION.py

--- ALL Tasks/FACE DETECTION AND  RECOGNITION.py
+++ ALL Tasks/FACE DETECTION AND  RECOGNITION.py
@@ -2,8 +2,23 @@ import cv2
 
 harcascade= "model/haarcascade_frontalface_default.xml"
 
-cap=cv2.VideoCapture(0)
+def detect_faces_image(image_path):
+    img = cv2.imread(image_path)
+    facecascade = cv2.CascadeClassifier(harcascade)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = facecascade.detectMultiScale(img_gray, 1.1, 4)
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        resized_img = cv2.resize(img, (800, 600))
+    
+    cv2.imshow("Detected Faces", resized_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+detect_faces_image(r"image_path")
 
+
+cap=cv2.VideoCapture(0)
 cap.set(3,640)  #width
 cap.set(4,480)  #height
 
@@ -25,3 +40,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
